8551de07b99ca30ce7f8ddbd36f6690113888d21
James Young aka solf1re2 <jy1v07@gmail.com>
2014-06-24 23:06:29 +0100

    initial commit

---
+++ desktop/src/com/solf1re/platformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.solf1re.platformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.solf1re.platformer.PlatformerStart;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PlatformerStart(), config);
+	}
+}
